3f19f12769feb1b7c526bc188eda432426b683b2
Sergey Sokolov <sokolovserej@gmail.com>
2025-05-27 14:42:52 +0300

    [ADD] wrap client

--- src/serejs/chroma-example.py
+++ src/serejs/chroma-example.py
@@ -1,17 +1,21 @@
 import chromadb
 
-client = chromadb.HttpClient(host='localhost', port=8000)
-client.heartbeat()
+if __name__ == '__main__':
+    client = chromadb.HttpClient(host='localhost', port=8000)
+    client.heartbeat()
 
-collection = client.create_collection("all-my-documents")
+    # collection = client.get_collection('all-my-documents')
+    collection = client.create_collection("all-my-documents")
 
-collection.add(
-    documents=["This is document1", "This is document2"],
-    metadatas=[{"source": "notion"}, {"source": "google-docs"}],
-    ids=["doc1", "doc2"], # unique for each doc
-)
+    collection.add(
+        documents=["This is document1", "This is document2"],
+        metadatas=[{"source": "notion"}, {"source": "google-docs"}],
+        ids=["doc13", "doc26"],  # unique for each doc
+    )
 
-results = collection.query(
-    query_texts=["This is a query document"],
-    n_results=2,
-)
+    results = collection.query(
+        query_texts=["This is a query document"],
+        n_results=2,
+    )
+
+    print(results)
