4a682820441b78e5d51ac0eb11841a1329557c68
Arttish <super.art-tish@yandex.ru>
2025-05-27 22:18:06 +0300

    [UPD] added Chroma documents load function updated model.py for better performance

--- src/arttish/hello.py
+++ src/arttish/hello.py
@@ -1,1 +1,8 @@
-print("hello world!")
+import os
+
+
+def parse_file_name(file_name):
+    return '.'.join(file_name.split("\\")[-1].split(".")[:-1])
+
+
+print(parse_file_name(os.path.join(os.getcwd(), 'hello.py')))
--- src/arttish/model.py
+++ src/arttish/model.py
@@ -6,25 +6,31 @@ import os
 model_repo = "gaianet/Qwen2.5-7B-Instruct-GGUF"
 model_basename = "Qwen2.5-7B-Instruct-Q4_K_M.gguf"
 
-model_path = "./models/" + model_basename
+model_path = "C:/Практика/etu-smartchat/src/models/" + model_basename
 
 if not os.path.exists(model_path):
     print(f"Downloading model {model_basename}")
     model_path = hf_hub_download(
         repo_id=model_repo,
         filename=model_basename,
-        local_dir="./models",
+        local_dir="C:/Практика/etu-smartchat/models",
     )
 else:
     print(f"Model {model_basename} is already downloaded.")
 
+llm = None
 
-llm = ChatLlamaCpp(
-    model_path=model_path,
-    temperature=0.1,
-    max_tokens=1024,
-    n_ctx=32768,
-    n_gpu_layers=40,
-    n_batch=512,
-    verbose=False,
-)
+
+def get_model():
+    global llm
+    if llm is None:
+        llm = ChatLlamaCpp(
+            model_path=model_path,
+            temperature=0.1,
+            max_tokens=1024,
+            n_ctx=32768,
+            n_gpu_layers=40,
+            n_batch=512,
+            verbose=False,
+        )
+    return llm
