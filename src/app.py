7a38a740614f281e9d1ed458a98131020ad02120
Arttish <super.art-tish@yandex.ru>
2025-05-29 15:15:11 +0300

    [FIX]es

--- src/app.py
+++ src/app.py
@@ -2,12 +2,4 @@ from src.model import init_model, get_model
 from src.bot import main
 
 if __name__ == '__main__':
-
-
     main()
-    # print(get_model().temperature)
-    # print('Type `!q` to exit')
-    # user_text = input()
-    # while user_text != '!q':
-    #     print(get_answer(user_text))
-    #     user_text = input()
