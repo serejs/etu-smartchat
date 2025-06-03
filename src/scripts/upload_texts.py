6f12a47759c1de435c1b5aba1851cd03e9d0a91e
Sergey Sokolov <sokolovserej@gmail.com>
2025-06-03 19:47:00 +0300

    [UPD] upload text from dirs

--- src/scripts/upload_texts.py
+++ src/scripts/upload_texts.py
@@ -4,16 +4,14 @@ import os
 import sys
 from os import environ
 from uuid import uuid4
-from dotenv import load_dotenv
 
 import numpy as np
-import whisper
 from chromadb import EmbeddingFunction, Embeddings, Documents
 from chromadb import HttpClient
+from dotenv import load_dotenv
 from langchain_core.documents import Document
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from yandex_cloud_ml_sdk import YCloudML
-from audio_extract import extract_audio
 
 argumentList = sys.argv[1:]
 options = "m:s:o:a:e"
@@ -21,7 +19,7 @@ long_options = ["meta", "size", "overlap", "audio", "env"]
 docs_metadata_json_path = None
 chunk_size = -1
 chunk_overlap = -1
-audio_dir = None
+text_dir = None
 
 try:
     # Parsing argument
@@ -35,29 +33,25 @@ try:
             chunk_size = int(currentValue)
         if currentArgument in ("-o", "--overlap"):
             chunk_overlap = int(currentValue)
-        if currentArgument in ("-a", "--audio"):
-            audio_dir = currentValue
+        if currentArgument in ("-t", "--text"):
+            text_dir = currentValue
         if currentArgument in ("-e", "--env"):
             load_dotenv(currentValue)
         else:
             load_dotenv()
-
-
 except getopt.error as err:
     print(str(err))
 
 if chunk_size <= 0 or chunk_overlap < 0 or chunk_size < chunk_overlap:
     raise ValueError("Invalid chunk size or overlap")
 
-if audio_dir is None:
-    raise ValueError("Audio dir is not set")
+if text_dir is None:
+    raise ValueError("Text dir is not set")
 
 sdk = YCloudML(
     folder_id=os.getenv("YANDEX_FOLDER_ID"),
     auth=os.getenv("YANDEX_AUTH")
 )
-
-model_type = os.getenv('WHISPER_MODEL', 'tiny')  # e.g. "tiny", "base", "small", "medium", "large"
 
 collection_name = os.getenv("CHROMA_COLLECTION")
 docs_embeddings = sdk.models.text_embeddings("doc")
@@ -67,46 +61,23 @@ class YaEmbeddings(EmbeddingFunction):
     def __call__(self, input: Documents) -> Embeddings:
         return [np.array(docs_embeddings.run(doc)) for doc in input]
 
+
 print(environ.get("CHROMA_HOST", 'localhost'), environ.get("CHROMA_PORT", "8000"))
 client = HttpClient(host=environ.get("CHROMA_HOST", 'localhost'), port=environ.get("CHROMA_PORT", "8000"))
 ef = YaEmbeddings()
 collection = client.get_or_create_collection(collection_name, embedding_function=ef)
 
-
-def convert_video_file(source_path, destination_path):
-    """Extract audio file to video file"""
-    extract_audio(
-        input_path=source_path,
-        output_path=destination_path,
-        overwrite=True,
-    )
-
-
-def video_to_audio(video_dir=audio_dir, audio_dir=audio_dir) -> None:
-    """Convert video files to audio files"""
-    print('Fetching video filenames...')
-    videos = [item for item in os.listdir(video_dir) if '.mp4' in item]
-    print('Fetched following videos: ', *videos, sep='\n- ', end='\n\n')
-
-    for video in videos:
-        source = os.path.join(video_dir, video)
-        dest = video.split('.mp4')[0] + '.mp3'
-        convert_video_file(source, os.path.join(audio_dir, dest))
-        print('-', dest, 'is converted')
-
-
 if __name__ == '__main__':
-    video_to_audio()
-    print('Fetching audio filenames...')
-    audios = [item for item in os.listdir(audio_dir) if '.mp3' in item]
-    print('Fetched following audios: ', *audios, sep='\n- ', end='\n\n')
-
-    asr_model = whisper.load_model(model_type)
-
     documents = []
-    for audio in audios:
-        text = asr_model.transcribe(os.path.join(audio_dir, audio))['text']
-        documents.append(Document(page_content=text, metadata={"source": audio}))
+    for filename in os.listdir(text_dir):
+        if not filename.endswith('.txt'): continue
+
+        file_path = os.path.join(text_dir, filename)
+
+        with open(file_path, 'r', encoding='utf-8') as file:
+            text = '\n'.join(file.readlines())
+
+        documents.append(Document(page_content=text, metadata={"source": text}))
 
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=chunk_size,
